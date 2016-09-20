0510243f20f8a690e450a00514736b4e1703aa66
saadaouad <aouadsaad05@gmail.com>
2016-09-20 01:06:55 +0100

    create admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+admin.site.register(Post)
+
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
 
 class Post(models.Model):
-	title = models.CharField()
+	title = models.CharField(max_length=140)
 	body = models.TextField()
 	date = models.DateTimeField()
 
